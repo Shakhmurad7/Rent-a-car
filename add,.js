04e4e7d96b920fbde08516eaa54f66b13386a44f
shakhmuad <muradb356@gmail.com>
2023-07-21 16:24:16 +0400

    add carusel

--- add,.js
+++ add,.js
@@ -1,1 +1,11 @@
 AOS.init();
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:4,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+
+    });
+  });
