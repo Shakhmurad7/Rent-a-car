518293d282554a24dcdc4360fa3050d77afa0810
shakhmurad <muradb356@gmail.com>
2023-07-23 02:53:00 +0400

    add accarrtion

--- add,.js
+++ add,.js
@@ -1,11 +1,15 @@
-AOS.init();
+const plus= document.getElementsByClassName('accardion-icon-plus')
+const minus = document.getElementsByClassName('accardion-icon-minus')
+const accardiontext = document.getElementsByClassName('accardion-p')
+const accardion = document.getElementsByClassName('accardion-blok')
 
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        items:4,
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:2000,
+ 
 
-    });
-  });
+for(i=0; i<accardion.length; i++){
+  accardion[i].addEventListener('click' , function(){
+    this.classList.toggle('accardion-active')
+  })      
+}  
+
+
+
