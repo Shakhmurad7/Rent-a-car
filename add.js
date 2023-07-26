15ede8269131c8d12faa35481590ac16ec30b631
shakhmuad <muradb356@gmail.com>
2023-07-26 14:44:53 +0400

    add Home page and reesponsiv

--- add.js
+++ add.js
@@ -8,15 +8,15 @@ for (i = 0; i < accardion.length; i++) {
 
 const menu = document.querySelector(".hamburger");
 const meniContainer = document.querySelector(".menu-container");
-const close = document.querySelector('.close')
+const close = document.querySelector(".close");
 menu.addEventListener("click", function () {
   meniContainer.classList.toggle("menu-active");
+  document.body.style.overflow = "hidden";
 });
-close.addEventListener('click', function(){
- meniContainer.classList.toggle("menu-active");
-})
- 
-
+close.addEventListener("click", function () {
+  meniContainer.classList.toggle("menu-active");
+  document.body.style.overflow = "visible";
+});
 
 fetch("http://localhost:3000/posts")
   .then((req) => req.json())
