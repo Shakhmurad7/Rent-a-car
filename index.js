ff327885c915e2cbebeb5016bbf0ca5da4b034e4
Shakhmurad <muradb356@gmail.com>
2023-08-01 00:50:53 +0400

    add swiper

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const swiper_thumbnail = new Swiper(".swiper_thumbnail", {
+  slidesPerView: 3,
+})
+const swiper = new Swiper('.swiper_main', {
+  loop: true,                         
+  autoplay: {                         
+      delay: 2000,  
+  },                   
+  navigation: {                       
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper_thumbnail,
+  },
+})
